b75e84e8ec77410e6bedacb8c603835da29e32f3
Federico Serafini <federico.serafini@studenti.unipr.it>
2022-10-22 15:30:54 +0200

    Small changes.

--- main.pl
+++ main.pl
@@ -1,32 +1,25 @@
 play :-
   write('\n'),
+  board_init(Board),
+  computer_turn(Board, _Board_out).
+
+board_init(Board) :-
   Board =
   [
     (1, 1, _), (1, 2, _), (1, 3, _),
     (2, 1, _), (2, 2, _), (2, 3, _),
     (3, 1, _), (3, 2, _), (3, 3, _)
-  ],
-  computer_turn(Board, _Board_out).
+  ].
 
 player :-
   write('\n************************\n\n'),
-  Board =
-  [
-    (1, 1, _), (1, 2, _), (1, 3, _),
-    (2, 1, _), (2, 2, _), (2, 3, _),
-    (3, 1, _), (3, 2, _), (3, 3, _)
-  ],
-  print_board(Board),
+  board_init(Board),
+  board_print(Board),
   player_turn(Board, _Board_out).
 
 computer :-
   write('\n************************\n\n'),
-  Board =
-  [
-    (1, 1, _), (1, 2, _), (1, 3, _),
-    (2, 1, _), (2, 2, _), (2, 3, _),
-    (3, 1, _), (3, 2, _), (3, 3, _)
-  ],
+  board_init(Board),
   computer_turn(Board, _Board_out).
 
 computer_turn(Board_in, Board_out) :-
@@ -34,7 +27,7 @@ computer_turn(Board_in, Board_out) :-
   ->
     board_list_val_max(Board_list, 'O', Board_out, _Val_out),
     writeln('Computer turn:'),
-    print_board(Board_out),
+    board_print(Board_out),
     ((member((_I1, _I2, X), Board_in), var(X))
     ->
       (win_chk(Board_out, 'O')
@@ -57,15 +50,15 @@ player_turn(Board_in, Board_out) :-
   ((member((_I1, _I2, V), Board_in), var(V))
   ->
     writeln('Your turn.'),
-    read_row(Row),
-    read_col(Col),
+    row_read(Row),
+    col_read(Col),
     Board_out = Board_in,
     (member((Row, Col, X), Board_out)
     ->
       (var(X)
       ->
         X = 'X',
-        print_board(Board_out),
+        board_print(Board_out),
         (win_chk(Board_out, 'X')
         ->
           writeln('You win!'),
@@ -225,25 +218,25 @@ diag_tris(Board, S) :-
     (X1_3 == S, X2_2 == S, X3_1 == S)
   ).
 
-print_board([]) :-
+board_print([]) :-
   !,
   write('\n\n').
-print_board([C1, C2, C3|R]) :-
-  print_row([C1, C2, C3]),
-  print_board(R).
+board_print([C1, C2, C3|R]) :-
+  row_print([C1, C2, C3]),
+  board_print(R).
 
-print_board_list([], _N) :-
+board_print_list([], _N) :-
   !.
-print_board_list([H|R], N) :-
+board_print_list([H|R], N) :-
   writeln(N),
-  print_board(H),
+  board_print(H),
   N_new is N+1,
-  print_board_list(R, N_new).
+  board_print_list(R, N_new).
 
-print_row([]) :-
+row_print([]) :-
   !,
   write('\n').
-print_row([H|R]) :-
+row_print([H|R]) :-
   H = (_I1, _I2, X),
   (var(X)
   ->
@@ -252,43 +245,52 @@ print_row([H|R]) :-
     write(X)
   ),
   write(' '),
-  print_row(R).
+  row_print(R).
 
-read_row(Row) :-
-  write('Insert row index: '),
+row_read(Row) :-
+  write('Insert row index and press enter (exit with \'q\'): '),
   read_atom(Row),
-  ((Row < 1 ; 3 < Row)
+  (Row == 'q'
   ->
-    writeln('Row index out of bounds.'),
-    read_row(_Row)
+    writeln('Bye!'),
+    halt
   ;
-    true
+    ((Row < 1 ; 3 < Row)
+    ->
+      writeln('Row index out of bounds.'),
+      row_read(_Row)
+    ;
+      true
+    )
   ).
 
-read_col(Col) :-
-  write('Insert col index: '),
+col_read(Col) :-
+  write('Insert col index and press enter (exit with \'q\'): '),
   read_atom(Col),
-  ((Col < 1 ; 3 < Col)
+  (Col == 'q'
   ->
-    writeln('Col index out of bounds.'),
-    read_col(_Col)
+    writeln('Bye!'),
+    halt
   ;
-    true
+    ((Col < 1 ; 3 < Col)
+    ->
+      writeln('Col index out of bounds.'),
+      col_read(_Col)
+    ;
+      true
+    )
   ).
 
-%%%% legge sequenza di caratteri termaxata da '\n' da std input,
-%%%% memorizza ASCII corrispondente in lista L
+read_atom(A) :-
+    read_ascii_list(L),
+    name(A, L).
+
 read_ascii_list(L) :-
     get0(C),
     read_ascii_list_loop(C, L).
 
+read_ascii_list_loop(10, []) :-
+  !.
 read_ascii_list_loop(C, [C|R]) :-
-    C \== 10, % codice ASCII per \n %
+    C \== 10,
     read_ascii_list(R).
-
-read_ascii_list_loop(10, []).
-
-%%%% traduce lista L di codici ASCII nell'atomo A corrispondente
-read_atom(A) :-
-    read_ascii_list(L),
-    name(A, L).
